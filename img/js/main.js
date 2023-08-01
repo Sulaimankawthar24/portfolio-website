ef22d6ddc85eb834e6cf5fdda0a236e18cbd98c9
kawthar sulaiman <sulaimankawthar24@gmail.com>
2023-08-01 16:11:55 -0700

    portfolio

---
+++ img/js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $('#navbar').on('click', function(event) {
+      if (this.hash !== '') {
+        event.preventDefault();
+
+        const hash = this.hash;
+
+        $('html, body').animate(
+          {
+            scrollTop: $(hash).offset().top - 100
+          },
+          800
+        );
+      }
+    });
+  });
